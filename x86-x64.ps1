80804a7706b56b8ab7430b892482b8ecfc0f4794
x0l4dn4 <danielcallejo.dev@gmail.com>
2026-03-29 18:18:57 +0200

    feat: xml target files manipulation

--- x86-x64.ps1
+++ x86-x64.ps1
@@ -8,7 +8,35 @@ param(
 	[string]$SolutionPath = "."
 )
 
+# First cleanup object and bin directories
 
-gci $SolutionPath -r -dir | ? { $_.Name -in "bin","obj" } | ri -r -fo  # You may have other aliases I just write them cuz who likes to write more???
+gci -Path $SolutionPath -Recurse -Directory |
+	Where-Object { $_.Name -in @("bin", "obj") } |
+	ri -Recurse -Force
 
-# gci -Path $SolutionPath -Recurse -Directory | Where-Object { $_.Name -in @("bin", "obj") } | ri -Recurse -Force
+# Target to x64 in project files, if you got a different file replace it
+# As project files are XML documents instead of deal with them as a raw text you can treat them like a tree of elements
+# Watch out! you may have profiles such as Debug with PlataformTarget set to x86 you may won't to edit them.
+
+gci -Recurse -Include *.csproj, *.vbproj | ForEach-Object {
+    [xml]$xml = Get-Content $_.FullName
+    $changed = $false
+
+    $xml.Project.PropertyGroup | ForEach-Object {
+        if ($_.PlatformTarget -eq "x86") {
+            $_.PlatformTarget = "x64"
+            $changed = $true
+        }
+    }
+
+    if ($changed) {
+        $xml.Save($_.FullName)
+        Write-Host "Updated: $($_.FullName)"
+    }
+}
+
+# TODO: Add login of the replaced lines
+
+
+# gci -Recurse -Include *.csproj, *.vbproj |  ForEach-Object { (Get-Content $_.FullName) -replace '<PlatformTarget>\s*x86\s*</PlatformTarget>', '<PlatformTarget>x64</PlatformTarget>' | Set-Content $_.FullName }
+
