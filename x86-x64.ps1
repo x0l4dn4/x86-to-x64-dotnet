a4655da8bd4f79a31a047fa7d334c4cc92cd3517
x0l4dn4 <danielcallejo.dev@gmail.com>
2026-03-29 17:25:18 +0200

    feat: dir params validation

--- x86-x64.ps1
+++ x86-x64.ps1
@@ -1,5 +1,14 @@
-param([string]$Path = ".")
+param(
+	 [ValidateScript({
+        	if (-not (Test-Path $_ -PathType Container)) {
+            		throw "Directory $_ does not exist. Make sure you are on the .sln directory."
+        	}
+        	$true
+    	})]
+	[string]$SolutionPath = "."
+)
 
-gci $Path -r -dir | ? { $_.Name -in "bin","obj" } | ri -r -fo  # You may have other aliases I just write them cuz who likes to write more???
 
-# gci -Path $Path -Recurse -Directory | Where-Object { $_.Name -in @("bin", "obj") } | ri -Recurse -Force
+gci $SolutionPath -r -dir | ? { $_.Name -in "bin","obj" } | ri -r -fo  # You may have other aliases I just write them cuz who likes to write more???
+
+# gci -Path $SolutionPath -Recurse -Directory | Where-Object { $_.Name -in @("bin", "obj") } | ri -Recurse -Force
